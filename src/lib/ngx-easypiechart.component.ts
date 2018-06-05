70271263ac25ec0036c07202eaa34092aa2de928
Karthik Kumar <allokarthik@gmail.com>
2018-06-05 15:27:02 +0530

    fix: directive

--- src/lib/ngx-easypiechart.component.ts
+++ src/lib/ngx-easypiechart.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'lib-ngx-easypiechart',
+  selector: 'ngx-easypiechart',
   template: `
     <p>
       ngx-easypiechart works!

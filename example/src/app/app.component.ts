4459991132bc47474d48a79c0f09af8a6ef988b2
Karthik K <allokarthik+code@gmail.com>
2018-06-06 15:11:34 +0530

    fix: example

--- example/src/app/app.component.ts
+++ example/src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  percent: number;
+  options: any;
+
+  constructor() {
+    this.percent = 80;
+    this.options = {
+      size: 50,
+      rotate: 0
+    };
+    const self = this;
+    setTimeout(
+      function() {
+        self.percent = 60;
+      }, 3000
+    );
+    setTimeout(
+      function() {
+        self.percent = 90;
+      }, 3000
+    );
+    setTimeout(
+      function() {
+        self.options = {
+          size: 100,
+          rotate: 0
+        };
+      },
+      4000
+    );
+  }
+
 }
--- example/src/app/app.module.ts
+++ example/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgxEasypiechartModule } from 'ngx-easypiechart';
+
+import {
+
+} from 'ngx-easypiechart';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxEasypiechartModule
   ],
   providers: [],
   bootstrap: [AppComponent]

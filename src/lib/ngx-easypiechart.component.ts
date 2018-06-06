d5e5bcb6781425f5ff4c0d97d0e4210755937f48
Karthik K <allokarthik+code@gmail.com>
2018-06-06 15:11:44 +0530

    Fix: plugin

--- src/lib/ngx-easypiechart.component.ts
+++ src/lib/ngx-easypiechart.component.ts
@@ -1,22 +1,51 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, ElementRef } from '@angular/core';
+declare var EasyPieChart: any;
 
 @Component({
   selector: 'ngx-easypiechart',
-  template: `
-    <p>
-      ngx-easypiechart works!
-    </p>
-  `,
-  styles: []
+  template: `<div>Loading ngx-easypiechart...</div>`
 })
-export class NgxEasypiechartComponent implements OnInit {
+export class NgxEasypiechartComponent implements OnInit, OnChanges {
 
   @Input()
   options: any;
 
-  constructor() { }
+  @Input()
+  percent: number;
+
+  private pieChart: any;
+
+  constructor(private element: ElementRef<HTMLDivElement>) {
+  }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    let optionsChanged = false;
+    if (changes.percent) {
+      this.percent = changes.percent.currentValue;
+    }
+    if (changes.options) {
+      this.options = changes.options.currentValue;
+      optionsChanged = true;
+    }
+    this.loadChart(optionsChanged);
+  }
+
+  loadChart(optionsChanged: boolean) {
+    if (this.options && this.percent) {
+      if (optionsChanged) {
+        const node = this.element.nativeElement;
+        while (node.hasChildNodes()) {
+          node.removeChild(node.lastChild);
+        }
+        this.pieChart = new EasyPieChart(node, this.options);
+      }
+      if (this.pieChart) {
+        this.pieChart.update(this.percent);
+      }
+    }
   }
 
 }
